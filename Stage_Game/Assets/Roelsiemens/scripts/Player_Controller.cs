c1f2be27fa2d11bfc240c8f7eb3a5ee38074451c
roelsiemens <roelsiemens@gmail.com>
2025-09-04 11:16:05 +0200

    player movement en camera follow

--- Stage_Game/Assets/Roelsiemens/scripts/Player_Controller.cs
+++ Stage_Game/Assets/Roelsiemens/scripts/Player_Controller.cs
@@ -2,15 +2,20 @@ using UnityEngine;
 
 public class Player_Controller : MonoBehaviour
 {
+    private CharacterController characterController;
+
+    public float speed = 5f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        characterController = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        characterController.Move(move * Time.deltaTime *speed);
     }
 }
